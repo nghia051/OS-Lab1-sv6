6cc4bbc3e753f80ada560c190d237c0ce4cb9c98
nghia051 <trungnghia05123@gmail.com>
2024-02-25 10:45:03 +0700

    update pingpong.c

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,18 +2,20 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     int p[2];
     pipe(p);
-    if(fork() == 0){
+    if (fork() == 0)
+    {
         char c;
         read(p[0], &c, 1);
         printf("%d: received ping\n", getpid());
-        write(p[1], "x", 1);
-    } else {
         write(p[1], "y", 1);
+    }
+    else
+    {
+        write(p[1], "x", 1);
         char c;
         read(p[0], &c, 1);
         printf("%d: received pong\n", getpid());
